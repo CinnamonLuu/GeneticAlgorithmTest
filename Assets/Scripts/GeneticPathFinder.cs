f7d53ed18d76451c5d983bc95f1579bd1e9946b2
CinnamonLuu <lucia.ortuno22@gmail.com>
2022-03-12 21:17:23 +0100

    First video

--- Assets/Scripts/GeneticPathFinder.cs
+++ Assets/Scripts/GeneticPathFinder.cs
@@ -10,24 +10,30 @@ public class GeneticPathFinder : MonoBehaviour
     public float pathMultiplier;
     int pathIndex = 0;
 
-    DNA dna;
-    bool hasFinished = false;
+    public DNA dna;
+    public bool hasFinished = false;
     bool hasBeenInitialized = false;
     Vector2 target;
     Vector2 nextPoint;
+
 
     public void InitCreature(DNA newDna, Vector2 target)
     {
         dna = newDna;
         this.target = target;
         nextPoint = transform.position;
+        hasBeenInitialized = true;
     }
 
     private void Update()
     {
-        if (hasBeenInitialized)
+        if (hasBeenInitialized && !hasFinished)
         {
-            if((Vector2)transform.position == nextPoint)
+            if (pathIndex == dna.genes.Count || Vector2.Distance(transform.position, target) < 0.5f)
+            {
+                hasFinished = true;
+            }
+            if ((Vector2)transform.position == nextPoint)
             {
                 nextPoint = ((Vector2)transform.position + dna.genes[pathIndex]);
                 pathIndex++;
@@ -36,6 +42,20 @@ public class GeneticPathFinder : MonoBehaviour
             {
                 transform.position = Vector2.MoveTowards(transform.position, nextPoint, creatureSpeed * Time.deltaTime);
             }
+        }
+    }
+
+
+    public float fitness
+    {
+        get
+        {
+            float dist = Vector2.Distance(transform.position, target);
+            if (dist == 0)
+            {
+                dist = 0.00001f;
+            }
+            return 60 / dist;
         }
     }
 }
--- Assets/Scripts/PopulationController.cs
+++ Assets/Scripts/PopulationController.cs
@@ -4,15 +4,101 @@ using UnityEngine;
 
 public class PopulationController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    List<GeneticPathFinder> population= new List<GeneticPathFinder>();
+    public GameObject creaturePrefab;
+    public int populationSize = 100;
+    public int genomeLenght;
+    public float cutoff = 0.3f;
+
+    public Transform spawnPoint;
+    public Transform end;
+
+    private void Start()
     {
-        
+        InitPopulation();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (!HasActive())
+        {
+            NextGeneration();
+        }
+    }
+
+    void InitPopulation()
+    {
+        for (int i = 0; i < populationSize; i++)
+        {
+            GameObject go = Instantiate(creaturePrefab, spawnPoint.position, Quaternion.identity);
+            GeneticPathFinder geneticPathFinder = go.GetComponent<GeneticPathFinder>();
+            geneticPathFinder.InitCreature(new DNA(genomeLenght), end.position);
+            population.Add(geneticPathFinder);
+        }
+    }
+
+    void NextGeneration()
+    {
+        int survivorCut = Mathf.RoundToInt(populationSize * cutoff);
+        List<GeneticPathFinder> survivors = new List<GeneticPathFinder>();
+
+        for (int i = 0; i < survivorCut; i++)
+        {
+            survivors.Add(GetFittest());
+        }
+        for (int i = 0; i < population.Count; i++)
+        {
+            Destroy(population[i].gameObject);
+        }
+        population.Clear();
+
+        while(population.Count < populationSize)
+        {
+            for (int i = 0; i < survivorCut; i++)
+            {
+                GameObject go = Instantiate(creaturePrefab, spawnPoint.position, Quaternion.identity);
+                GeneticPathFinder geneticPathFinder = go.GetComponent<GeneticPathFinder>();
+                geneticPathFinder.InitCreature(new DNA(survivors[i].dna, survivors[Random.Range(0, 10)].dna), end.position);
+                population.Add(geneticPathFinder);
+                if(population.Count >= populationSize)
+                {
+                    break;
+                }
+            }
+        }
+
+        for (int i = 0; i < survivors.Count; i++)
+        {
+            Destroy(survivors[i].gameObject);
+        }
+    }
+
+    GeneticPathFinder GetFittest()
+    {
+        float maxFitness = float.MinValue;
+        int index = 0;
+        for (int i = 0; i < population.Count; i++)
+        {
+            if(population[i].fitness > maxFitness)
+            {
+                maxFitness = population[i].fitness;
+                index = i;
+            }
+        }
+        GeneticPathFinder fittest = population[index];
+        population.Remove(fittest);
+        return fittest;
+    }
+
+    bool HasActive()
+    {
+        for (int i = 0; i < population.Count; i++)
+        {
+            if (!population[i].hasFinished)
+            {
+                return true;
+            }
+        }
+        return false;
     }
 }
