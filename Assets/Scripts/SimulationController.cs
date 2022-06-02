fb28857f5c2516c1b499329c188207838fda899c
Joymg <joy.marquezg@gmail.com>
2022-06-02 20:39:50 +0200

    Import assets and create spawn and target prefab

--- Assets/Scripts/SimulationController.cs
+++ Assets/Scripts/SimulationController.cs
@@ -30,6 +30,11 @@ public class SimulationController : MonoBehaviour
     public SimulationMap map;
     /*--------------------------------------------------------------- */
 
+    //TEMPORAL
+    public List<Line> tmpLines = new List<Line>();
+    public List<DNA_DataSimulation> temporalGPUValidator = new List<DNA_DataSimulation>();
+
+
     public void Awake()
     {
         if (instance == null)
@@ -57,10 +62,42 @@ public class SimulationController : MonoBehaviour
 
     private void InitializeCPUSimulation()
     {
+
         SceneManager.LoadScene(visualSimulationSceneIndex, LoadSceneMode.Additive);
     }
+   
     private void InitializeGPUSimulation()
     {
+
         SceneManager.LoadScene(dataSimulationSceneIndex, LoadSceneMode.Additive);
+
+
+        /*mapSerializer = FindObjectOfType<MapSerializer>();
+        mapSerializer.Init();
+
+        intersectionChecker = new GPUIntersectionChecker();
+
+        List<Line> tmpLines2 = new List<Line>();
+
+        Temporal
+        manhattanPopulationController = new GameObject().AddComponent<PopulationController>();
+        manhattanPopulationController.populationSize = NumAgents;
+        manhattanPopulationController.genomeLenght = NumMovements;
+        manhattanPopulationController.spawnPoint = GameObject.FindGameObjectWithTag("Spawn").transform;
+        manhattanPopulationController.end = GameObject.FindGameObjectWithTag("Target").transform;
+
+        for (int i = 0; i < NumAgents; i++)
+        {
+            temporalGPUValidator.Add(new GPUDna(mapSerializer.spawnPosition.position, NumMovements));
+            tmpLines.AddRange(temporalGPUValidator[i].lines);
+            tmpLines2.Add(new Line(new Vector2(0, i - 4), new Vector2(0, i - 3)));
+        }
+        intersectionChecker.Init(NumAgents, mapSerializer.ObastacleMapLines.ToArray(), tmpLines.ToArray());
+        intersectionChecker.Init(NumAgents, mapSerializer.ObastacleMapLines.ToArray(), tmpLines2.ToArray());
+
+        manhattanPopulationController.InitPopulation(true);*/
+
+
+
     }
 }
